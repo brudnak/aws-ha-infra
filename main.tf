8e5028dd3c34d732f41a71afaed62e1e03ca159b
Andrew Brudnak <brudnak@protonmail.com>
2022-04-05 12:45:37 -0700

    🟣 Reproduction & Validation TF HA

--- main.tf
+++ main.tf
@@ -24,6 +24,23 @@ module "reproduction" {
   aws_pem_key_name      = var.aws_pem_key_name
 }
 
+module "validation" {
+  source                = "./modules/validation"
+  aws_prefix            = var.aws_prefix
+  aws_region            = var.aws_region
+  aws_access_key        = var.aws_access_key
+  aws_secret_key        = var.aws_secret_key
+  aws_vpc               = var.aws_vpc
+  aws_subnet_a          = var.aws_subnet_a
+  aws_subnet_b          = var.aws_subnet_b
+  aws_subnet_c          = var.aws_subnet_c
+  aws_ami               = var.aws_ami
+  aws_subnet_id         = var.aws_subnet_id
+  aws_security_group_id = var.aws_security_group_id
+  aws_pem_key_name      = var.aws_pem_key_name
+}
+
+
 variable "aws_prefix" {}
 variable "aws_region" {}
 variable "aws_access_key" {}
@@ -39,4 +56,8 @@ variable "aws_pem_key_name" {}
 
 output "reproduction_module_output" {
   value = module.reproduction
+}
+
+output "validation_module_output" {
+  value = module.validation
 }
---
+++ modules/validation/main.tf
@@ -0,0 +1,111 @@
+resource "random_pet" "random_pet" {
+
+  keepers = {
+    aws_prefix = "${var.aws_prefix}"
+  }
+
+  length    = 2
+  separator = "-"
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "aws_instance" {
+  count                  = 3
+  ami                    = var.aws_ami
+  instance_type          = "t3.2xlarge"
+  subnet_id              = var.aws_subnet_id
+  vpc_security_group_ids = [var.aws_security_group_id]
+  key_name               = var.aws_pem_key_name
+
+  root_block_device {
+    volume_size = 150
+  }
+
+  tags = {
+    Name = "${random_pet.random_pet.keepers.aws_prefix}-${random_pet.random_pet.id}"
+  }
+}
+
+
+resource "aws_lb_target_group" "aws_lb_target_group_80" {
+  name        = "${var.aws_prefix}-80-${random_pet.random_pet.id}"
+  port        = 80
+  protocol    = "TCP"
+  target_type = "instance"
+  vpc_id      = var.aws_vpc
+  health_check {
+    protocol          = "TCP"
+    port              = "traffic-port"
+    healthy_threshold = 3
+    interval          = 10
+  }
+}
+
+resource "aws_lb_target_group" "aws_lb_target_group_443" {
+  name        = "${var.aws_prefix}-443-${random_pet.random_pet.id}"
+  port        = 443
+  protocol    = "TCP"
+  target_type = "instance"
+  vpc_id      = var.aws_vpc
+  health_check {
+    protocol          = "TCP"
+    port              = 443
+    healthy_threshold = 3
+    interval          = 10
+  }
+}
+
+# attach instances to the target group 80
+resource "aws_lb_target_group_attachment" "attach_tg_80" {
+  count            = length(aws_instance.aws_instance)
+  target_group_arn = aws_lb_target_group.aws_lb_target_group_80.arn
+  target_id        = aws_instance.aws_instance[count.index].id
+  port             = 80
+}
+
+# attach instances to the target group 443
+resource "aws_lb_target_group_attachment" "attach_tg_443" {
+  count            = length(aws_instance.aws_instance)
+  target_group_arn = aws_lb_target_group.aws_lb_target_group_443.arn
+  target_id        = aws_instance.aws_instance[count.index].id
+  port             = 443
+}
+
+
+# create a load balancer
+resource "aws_lb" "aws_lb" {
+  load_balancer_type = "network"
+  name               = "${var.aws_prefix}-nlb-${random_pet.random_pet.id}"
+  internal           = false
+  ip_address_type    = "ipv4"
+  subnets            = [var.aws_subnet_a, var.aws_subnet_b, var.aws_subnet_c]
+}
+
+# add a listener for port 80
+resource "aws_lb_listener" "aws_lb_listener_80" {
+  load_balancer_arn = aws_lb.aws_lb.arn
+  port              = "80"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.aws_lb_target_group_80.arn
+  }
+}
+
+# add a listener for port 443
+resource "aws_lb_listener" "aws_lb_listener_443" {
+  load_balancer_arn = aws_lb.aws_lb.arn
+  port              = "443"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.aws_lb_target_group_443.arn
+  }
+}
