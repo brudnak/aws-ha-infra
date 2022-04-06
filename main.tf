64f6f7cec8f7371c985c282d36d7ddbb4b88c1cd
Andrew Brudnak <brudnak@protonmail.com>
2022-04-05 22:33:11 -0700

    🟣 Head & latest release added

--- main.tf
+++ main.tf
@@ -40,6 +40,37 @@ module "validation" {
   aws_pem_key_name      = var.aws_pem_key_name
 }
 
+module "latest" {
+  source                = "./modules/latest"
+  aws_prefix            = var.aws_prefix
+  aws_region            = var.aws_region
+  aws_access_key        = var.aws_access_key
+  aws_secret_key        = var.aws_secret_key
+  aws_vpc               = var.aws_vpc
+  aws_subnet_a          = var.aws_subnet_a
+  aws_subnet_b          = var.aws_subnet_b
+  aws_subnet_c          = var.aws_subnet_c
+  aws_ami               = var.aws_ami
+  aws_subnet_id         = var.aws_subnet_id
+  aws_security_group_id = var.aws_security_group_id
+  aws_pem_key_name      = var.aws_pem_key_name
+}
+
+module "latest" {
+  source                = "./modules/head"
+  aws_prefix            = var.aws_prefix
+  aws_region            = var.aws_region
+  aws_access_key        = var.aws_access_key
+  aws_secret_key        = var.aws_secret_key
+  aws_vpc               = var.aws_vpc
+  aws_subnet_a          = var.aws_subnet_a
+  aws_subnet_b          = var.aws_subnet_b
+  aws_subnet_c          = var.aws_subnet_c
+  aws_ami               = var.aws_ami
+  aws_subnet_id         = var.aws_subnet_id
+  aws_security_group_id = var.aws_security_group_id
+  aws_pem_key_name      = var.aws_pem_key_name
+}
 
 variable "aws_prefix" {}
 variable "aws_region" {}
@@ -60,4 +91,12 @@ output "reproduction_module_output" {
 
 output "validation_module_output" {
   value = module.validation
+}
+
+output "latest_module_output" {
+  value = module.latest
+}
+
+output "head_module_output" {
+  value = module.latest
 }
---
+++ modules/head/outputs.tf
@@ -0,0 +1,14 @@
+output "instance_public_ip" {
+  value = [for instance in aws_instance.aws_instance : instance.public_ip]
+}
+output "instance_private_ip" {
+  value = [for instance in aws_instance.aws_instance : instance.private_ip]
+}
+
+output "random_pet_id_for_head" {
+  value = random_pet.random_pet.id
+}
+
+output "head_load_balancer_dns_name" {
+  value = aws_lb.aws_lb.dns_name
+}
