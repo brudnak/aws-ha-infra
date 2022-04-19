590acdb4ca8fc71aa0e403d41f432ec730964cd3
Andrew Brudnak <brudnak@protonmail.com>
2022-04-19 09:58:19 -0700

    💙 Adding for rke imports

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
-output "Reproduction" {
+output "reproduction" {
   value = module.high-availability-infrastructure-reproduction
 }
 
-output "Validation" {
+output "rke-import-repro" {
+  value = module.high-availability-infrastructure-rke-import-repro
+}
+
+output "validation" {
   value = module.high-availability-infrastructure-validation
+}
+
+output "rke-import-valid" {
+  value = module.high-availability-infrastructure-rke-import-valid
 }
