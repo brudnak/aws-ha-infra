b5edaae5f5e6042215c609b1cc265fe46a3c324d
Andrew Brudnak <brudnak@protonmail.com>
2022-04-22 09:12:32 -0700

    💙 Cleanup

--- main.tf
+++ main.tf
@@ -24,41 +24,8 @@ module "high-availability-infrastructure-reproduction" {
   aws_pem_key_name      = var.aws_pem_key_name
 }
 
-# RKE Import Cluster for Reproduction server.
-module "high-availability-infrastructure-rke-import-repro" {
-  source                = "./modules/aws-high-availability-infrastructure"
-  aws_prefix            = var.aws_prefix
-  aws_access_key        = var.aws_access_key
-  aws_secret_key        = var.aws_secret_key
-  aws_vpc               = var.aws_vpc
-  aws_subnet_a          = var.aws_subnet_a
-  aws_subnet_b          = var.aws_subnet_b
-  aws_subnet_c          = var.aws_subnet_c
-  aws_ami               = var.aws_ami
-  aws_subnet_id         = var.aws_subnet_id
-  aws_security_group_id = var.aws_security_group_id
-  aws_pem_key_name      = var.aws_pem_key_name
-}
-
-
 # Validation HA Rancher Server
 module "high-availability-infrastructure-validation" {
-  source                = "./modules/aws-high-availability-infrastructure"
-  aws_prefix            = var.aws_prefix
-  aws_access_key        = var.aws_access_key
-  aws_secret_key        = var.aws_secret_key
-  aws_vpc               = var.aws_vpc
-  aws_subnet_a          = var.aws_subnet_a
-  aws_subnet_b          = var.aws_subnet_b
-  aws_subnet_c          = var.aws_subnet_c
-  aws_ami               = var.aws_ami
-  aws_subnet_id         = var.aws_subnet_id
-  aws_security_group_id = var.aws_security_group_id
-  aws_pem_key_name      = var.aws_pem_key_name
-}
-
-# RKE Import Cluster for Validation server.
-module "high-availability-infrastructure-rke-import-valid" {
   source                = "./modules/aws-high-availability-infrastructure"
   aws_prefix            = var.aws_prefix
   aws_access_key        = var.aws_access_key
--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,6 @@ output "reproduction" {
   value = module.high-availability-infrastructure-reproduction
 }
 
-output "rke-import-repro" {
-  value = module.high-availability-infrastructure-rke-import-repro
-}
-
 output "validation" {
   value = module.high-availability-infrastructure-validation
-}
-
-output "rke-import-valid" {
-  value = module.high-availability-infrastructure-rke-import-valid
 }
