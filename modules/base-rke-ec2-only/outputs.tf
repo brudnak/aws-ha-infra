53590d974482f03bfc1e3c4d6064a082205b92be
Andrew Brudnak <brudnak@protonmail.com>
2022-04-25 23:57:21 -0700

    🟪 Adding standalone rke1 module

---
+++ modules/base-rke-ec2-only/outputs.tf
@@ -0,0 +1,10 @@
+output "instance_public_ip" {
+  value = [for instance in aws_instance.aws_instance : "public IP for rke config: ${instance.public_ip}"]
+}
+output "instance_private_ip" {
+  value = [for instance in aws_instance.aws_instance : "private IP for rke config: ${instance.private_ip}"]
+}
+
+output "random_pet_id" {
+  value = "random ID to identify aws resources: ${random_pet.random_pet.id}"
+}
