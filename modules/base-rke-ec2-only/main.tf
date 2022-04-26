c937d07756ad523ff7b8ac2b8e294b4715afe92f
Andrew Brudnak <brudnak@protonmail.com>
2022-04-25 23:57:21 -0700

    🟪 Adding standalone rke1 module

---
+++ modules/base-rke-ec2-only/main.tf
@@ -0,0 +1,33 @@
+resource "random_pet" "random_pet" {
+
+  keepers = {
+    aws_prefix = "${var.aws_prefix}"
+  }
+
+  length    = 2
+  separator = "-"
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+
+resource "aws_instance" "aws_instance" {
+  count                  = 3
+  ami                    = var.aws_ami
+  instance_type          = "t3.2xlarge"
+  subnet_id              = var.aws_subnet_id
+  vpc_security_group_ids = [var.aws_security_group_id]
+  key_name               = var.aws_pem_key_name
+
+  root_block_device {
+    volume_size = 150
+  }
+
+  tags = {
+    Name = "${random_pet.random_pet.keepers.aws_prefix}-${random_pet.random_pet.id}"
+  }
+}
