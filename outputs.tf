5052bbcd209a6f77e9d1c4144d6c518536769bfd
Andrew Brudnak <brudnak@protonmail.com>
2022-07-18 20:10:58 -0700

    💙 AWS Infra

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,15 @@ output "reproduction" {
 output "validation" {
   value = module.high-availability-infrastructure-validation
 }
+
+output "rke-standalone-1" {
+  value = module.rke-standalone-1
+}
+
+output "rke-standalone-2" {
+  value = module.rke-standalone-2
+}
+
+output "rke-standalone-3" {
+  value = module.rke-standalone-3
+}
