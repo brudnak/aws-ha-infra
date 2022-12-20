280143fcf051e006fb307fdcd83b0495b4e34cae
andrew brudnak <brudnak@protonmail.com>
2022-12-20 09:05:48 -0700

    🟨 ec2 size outputs

--- outputs.tf
+++ outputs.tf
@@ -5,15 +5,3 @@ output "ha-1" {
 output "ha-2" {
   value = module.ha-2
 }
-
-output "ha-3" {
-  value = module.ha-3
-}
-
-output "ha-4" {
-  value = module.ha-4
-}
-
-output "ha-5" {
-  value = module.ha-5
-}
